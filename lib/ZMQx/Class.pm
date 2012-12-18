86761857e4cff4f309b8fcfb8f442ec4d1e6a8a2
Thomas Klausner <domm@plix.at>
2012-12-18 10:43:23 +0100

    adapted code to new name

--- lib/ZMQx/Class.pm
+++ lib/ZMQx/Class.pm
@@ -2,7 +2,7 @@ package ZMQx::Class;
 use strict;
 use warnings;
 use 5.010;
-use ZMQx::Helper::Socket;
+use ZMQx::Class::Socket;
 
 use ZMQ::LibZMQ3 qw(zmq_socket zmq_init);
 use ZMQ::Constants qw(ZMQ_REQ ZMQ_REP ZMQ_DEALER ZMQ_ROUTER ZMQ_PULL ZMQ_PUSH ZMQ_PUB ZMQ_SUB  ZMQ_XPUB ZMQ_XSUB ZMQ_PAIR);
@@ -24,7 +24,7 @@ my %types = (
 sub socket {
     my ($class, $context, $type, $connect, $address ) = @_;
     die "no such socket type: $type" unless defined $types{$type};
-    my $socket = ZMQx::Helper::Socket->new(
+    my $socket = ZMQx::Class::Socket->new(
         socket=>zmq_socket($context,$types{$type}),
     );
     if ($connect && $address) {
