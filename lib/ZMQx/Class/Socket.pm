780f6e010ff9b2107017b4356368e16c40c50cf6
Thomas Klausner <domm@plix.at>
2013-02-19 22:02:56 +0100

    warn when trying to set a sockopt after connecting

--- lib/ZMQx/Class/Socket.pm
+++ lib/ZMQx/Class/Socket.pm
@@ -6,7 +6,7 @@ use 5.010;
 # ABSTRACT: A ZMQ Socket
 
 use Moose;
-use Carp qw(croak);
+use Carp qw(croak carp);
 use namespace::autoclean;
 use Package::Stash;
 use ZMQ::LibZMQ3;
@@ -143,25 +143,53 @@ sub get_fh {
 
 {
     no strict 'refs';
-    my @sockopt_constants=qw(ZMQ_SNDHWM ZMQ_RCVHWM ZMQ_AFFINITY ZMQ_SUBSCRIBE ZMQ_UNSUBSCRIBE ZMQ_IDENTITY ZMQ_RATE ZMQ_RECOVERY_IVL ZMQ_SNDBUF ZMQ_RCVBUF ZMQ_LINGER ZMQ_RECONNECT_IVL ZMQ_RECONNECT_IVL_MAX ZMQ_BACKLOG ZMQ_MAXMSGSIZE ZMQ_MULTICAST_HOPS ZMQ_RCVTIMEO ZMQ_SNDTIMEO ZMQ_IPV4ONLY);
-    my $stash = Package::Stash->new(__PACKAGE__);
-    foreach my $const (@sockopt_constants) {
-        my $get = my $set = lc($const);
-        $set =~s/^zmq_/set_/;
-        $get =~s/^zmq_/get_/;
+    my @sockopts_before_connect = qw(ZMQ_SNDHWM ZMQ_RCVHWM ZMQ_AFFINITY ZMQ_IDENTITY ZMQ_RATE ZMQ_RECOVERY_IVL ZMQ_SNDBUF ZMQ_RCVBUF ZMQ_RECONNECT_IVL ZMQ_RECONNECT_IVL_MAX ZMQ_BACKLOG ZMQ_MAXMSGSIZE ZMQ_MULTICAST_HOPS ZMQ_RCVTIMEO ZMQ_SNDTIMEO ZMQ_IPV4ONLY);
 
-        if ($stash->has_symbol('&'.$const)) {
-            my $constval = &$const;
+    my @sockopts_after_connect = qw(ZMQ_SUBSCRIBE ZMQ_UNSUBSCRIBE ZMQ_LINGER ZMQ_ROUTER_MANDATORY ZMQ_XPUB_VERBOSE);
+
+    my $stash = Package::Stash->new(__PACKAGE__);
+    foreach my $const (@sockopts_before_connect) {
+        _setup_sockopt_helpers($const, $stash, 1);
+    }
+    foreach my $const (@sockopts_after_connect) {
+        _setup_sockopt_helpers($const, $stash, 0);
+    }
+
+}
+
+sub _setup_sockopt_helpers {
+    my ($const, $stash, $set_only_before_connect) = @_;
+    my $get = my $set = lc($const);
+    $set =~s/^zmq_/set_/;
+    $get =~s/^zmq_/get_/;
+    no strict 'refs';
+
+    if ($stash->has_symbol('&'.$const)) {
+        my $constval = &$const;
+        if ($set_only_before_connect) {
+            $stash->add_symbol('&'.$set => sub {
+                my $self = shift;
+                if ($self->_connected) {
+                    carp "Setting '$const' only works before connect/bind. Value not stored!";
+                }
+                else {
+                    zmq_setsockopt($self->socket,$constval,@_);
+                }
+                return $self;
+            });
+        }
+        else {
             $stash->add_symbol('&'.$set => sub {
                 my $self = shift;
                 zmq_setsockopt($self->socket,$constval,@_);
                 return $self;
             });
-            $stash->add_symbol('&'.$get => sub {
-                my $self = shift;
-                return zmq_getsockopt($self->socket,$constval);
-            });
         }
+        $stash->add_symbol('&'.$get => sub {
+            my $self = shift;
+            return zmq_getsockopt($self->socket,$constval);
+        });
     }
 }
+
 1;
